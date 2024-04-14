7be6c97718001ad0d0e4a354e00a00b293d96f64
itssohel910 <163528600+itssohel910@users.noreply.github.com>
2024-04-14 18:05:35 +0600

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,1 @@
-alert("Warning: This site may contain suicide-related content. If you're struggling, please reach out for help.");
+alert("Warning: This site may contain suicide-related content. Avoid it to stay alive");
